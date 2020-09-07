2415098f76af500d8912e1d2375d27c8c5f61814
SSAAMMAA <facundo.samartino@gmail.com>
2020-09-07 15:11:34 -0300

    Se agregan detalles de estilo, funcionamiento de la descarga y el borrado del lienzo

--- TP1/ENTREGABLE/js/script.js
+++ TP1/ENTREGABLE/js/script.js
@@ -1,20 +1,20 @@
-let paintTool = (tool, ctx)=>{
+let paintTool = (tool, ctx) => {
     let c = document.querySelector("#canvas");
     let isPainting = false;
-        
-    c.onmousedown = function (e){
+
+    c.onmousedown = function (e) {
         isPainting = true;
-        if(tool){
+        if (tool) {
             ctx.moveTo(e.pageX - c.offsetLeft, e.pageY - c.offsetTop);
         }
-    }   
+    }
 
-    c.onmouseup = function(){
+    c.onmouseup = function () {
         isPainting = false;
         ctx.beginPath();
     }
-    
-    c.onmousemove = function(e){
+
+    c.onmousemove = function (e) {
         if (isPainting) {
             if (tool) {
                 ctx.lineTo(e.pageX - c.offsetLeft, e.pageY - c.offsetTop);
@@ -22,25 +22,32 @@ let paintTool = (tool, ctx)=>{
                 ctx.strokeStyle = document.querySelector("#muestrario").value;
                 ctx.stroke();
             }
-            else{
+            else {
                 ctx.beginPath();
                 ctx.clearRect(e.pageX - c.offsetLeft, e.pageY - c.offsetTop, document.querySelector("#range").value, document.querySelector("#range").value);
             }
         }
     }
-    
-    c.onmouseout = function(){
+
+    c.onmouseout = function () {
         isPainting = false;
     };
 }
 
-function actualizarPrimero(event) {
-    var p = document.querySelector("p");
-  
-    if (p) {
-      p.style.color = event.target.value;
-    }
-  }
+let deleteAll = (ctx) => {
+    ctx.save();
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.restore();
+}
+
+let downloadImg = () => {
+    let link = document.querySelector("#download");
+    let filename = prompt("Guardar como...", "Nombre del archivo");
+    filename = filename + ".jpg";
+    link.href = document.querySelector("#canvas").toDataURL("image/jpg");
+    link.download = filename;
+}
 
 let loadPage = () => {
 
@@ -49,13 +56,17 @@ let loadPage = () => {
 
     let width = document.querySelector("#canvas").width;
     let height = document.querySelector("#canvas").height;
+
     let imageData = ctx.createImageData(width, height);
 
     let btnPencil = document.querySelector("#btnPencil");
-    btnPencil.addEventListener("click", function(e){paintTool(true,ctx)});
+    btnPencil.addEventListener("click", function (e) { paintTool(true, ctx) });
     let btnEraser = document.querySelector("#btnEraser");
-    btnEraser.addEventListener("click", function(e){paintTool(false,ctx)});
+    btnEraser.addEventListener("click", function (e) { paintTool(false, ctx) });
+    let btnDelete = document.querySelector("#btnDelete");
+    btnDelete.addEventListener("click", function (e) { deleteAll(ctx) });
+    let btnDownload = document.querySelector("#btnDownload");
+    btnDownload.addEventListener("click", downloadImg);
 
-    
 }
 document.addEventListener("DOMContentLoaded", loadPage);
