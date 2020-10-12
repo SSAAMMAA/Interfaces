869ccfd99b34e714bb6cf89bf9488b6bcab469f9
SSAAMMAA <facundo.samartino@gmail.com>
2020-10-12 14:49:57 -0300

    Se agrega menu hamburguesa y cuenta atras

--- TP3/Entregable/js/script.js
+++ TP3/Entregable/js/script.js
@@ -15,3 +15,41 @@ window.addEventListener("mousemove", (e) => {
     img.style.transformStyle = `preserve-3d`;
     img.style.transform = `scale(0.8) translate3d(0,0,-100px) rotateY(${horizontalFactor}deg) rotateX(${verticalFactor}deg)`;
 });
+
+// let loading = () =>{
+
+// }
+
+//COUNTDOWN
+let countDownDate = new Date("Dec 10, 2020 18:00:00").getTime();
+
+let x = setInterval(function() {
+
+  let now = new Date().getTime();
+
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.querySelector("#countdown").innerHTML = "El estreno sera en " + days + " dias, " + hours + " horas, "
+  + minutes + " minutos, " + seconds + " segundos";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.querySelector("#countdown").innerHTML = "Ya podes ir a buscar tu entrada";
+  }
+}, 1000);
+
+//MENU HAMBURGESA
+function abrirMenu() {
+    let x = document.querySelector("#myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  document.querySelector("#menu-input").addEventListener("click",abrirMenu);
